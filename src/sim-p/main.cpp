c81d4a9f137df4377f46d713000fe8c8d8a3cd42
Demetrius Comes <cmdc0dez@gmail.com>
2024-01-31 22:23:41 -0700

    added command line to pass tests

--- src/sim-p/main.cpp
+++ src/sim-p/main.cpp
@@ -1,13 +1,26 @@
 
 #if 1
+
+#include <CLI/CLI.hpp>
 #include "app.h"
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <spdlog/spdlog.h>
+#include <internal_use_only/config.hpp>
 
-int main([[maybe_unused]] int arc, [[maybe_unused]]  char *agrv[]) {
-	App MyApp;
-	try {
+int main([[maybe_unused]] int argc, [[maybe_unused]]  char *argv[]) {
+   App MyApp;
+   try {
+      CLI::App app{ fmt::format("{} version {}", SimulationPlayground::cmake::project_name, SimulationPlayground::cmake::project_version) };
+      bool show_version = false;
+      app.add_flag("--version", show_version, "Show version information");
+      CLI11_PARSE(app, argc, argv);
+
+      if (show_version) {
+         fmt::print("{}\n", SimulationPlayground::cmake::project_version);
+         return EXIT_SUCCESS;
+      }
         MyApp.run();
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
